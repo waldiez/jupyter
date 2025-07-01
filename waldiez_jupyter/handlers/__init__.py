4bd01a845cad2226d5206c737538810762dde611
Lazaros Toumanidis <laztoum@protonmail.com>
2025-07-01 11:47:09 +0300

    update static files handler

--- waldiez_jupyter/handlers/__init__.py
+++ waldiez_jupyter/handlers/__init__.py
@@ -3,8 +3,11 @@
 
 """Extension handlers for the Jupyter Server."""
 
+import os
+
 from jupyter_server.serverapp import ServerWebApplication
 from jupyter_server.utils import url_path_join
+from tornado import web
 
 from .extra_static_files import ensure_extra_static_files
 from .files_handler import FilesHandler
@@ -29,7 +32,17 @@ def setup_handlers(web_app: ServerWebApplication) -> None:
     base_url = web_app.settings["base_url"]
     files_pattern = url_path_join(base_url, "waldiez", "files")
     upload_pattern = url_path_join(base_url, "waldiez", "upload")
+    min_maps_pattern = url_path_join(base_url, "min-maps")
+    min_maps_path = os.path.join(static_path, "min-maps")
     web_app.add_handlers(
         host_pattern,
-        [(files_pattern, FilesHandler), (upload_pattern, UploadHandler)],
+        [
+            (files_pattern, FilesHandler),
+            (upload_pattern, UploadHandler),
+            (
+                rf"{min_maps_pattern}/(.*)",
+                web.StaticFileHandler,
+                {"path": min_maps_path},
+            ),
+        ],
     )
